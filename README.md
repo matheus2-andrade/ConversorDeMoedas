4c28c892384dc0ae1830fd38199ad0dfeb7ab028
matheus2-andrade <mthrafaelandrade@gmail.com>
2021-04-01 01:55:15 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto desenvolvido em dart/flutter fazendo uma conversão de moedas em tempo real com dados
+recebidos de um API.
---
+++ android/app/src/main/java/andrade/com/conversor_de_moedas/MainActivity.java
@@ -0,0 +1,6 @@
+package andrade.com.conversor_de_moedas;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
